2dbf2818a8452c2272f89ee7b6be61ffd2943582
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-05-17 23:22:48 +0100

    fixed request error for when anki hasnt started

--- anki_connect.py
+++ anki_connect.py
@@ -1,25 +1,28 @@
 import json
 import urllib.request
 
-
-def request(action, **params):
-    return {'action': action, 'params': params, 'version': 6}
-
-
-def invoke(action, **params):
-    requestJson = json.dumps(request(action, **params)).encode('utf-8')
-    response = json.load(urllib.request.urlopen(
-        urllib.request.Request('http://localhost:8765', requestJson)))
-    if len(response) != 2:
-        raise Exception('response has an unexpected number of fields')
-    if 'error' not in response:
-        raise Exception('response is missing required error field')
-    if 'result' not in response:
-        raise Exception('response is missing required result field')
-    if response['error'] is not None:
-        raise Exception(response['error'])
-    return response['result']
+class AnkiConnect:
+	def request(self, action, **params):
+		return {'action': action, 'params': params, 'version': 6}
 
 
-result = invoke('deckNames')
+	def invoke(self, action, **params):
+		requestJson = json.dumps(self.request(action, **params)).encode('utf-8')
+		try:
+			response = json.load(urllib.request.urlopen(urllib.request.Request('http://localhost:8765', requestJson)))
+		except urllib.error.URLError:
+			print("Error! Make sure the Anki application is running and the \"Ankiconect\" pluging is installed")
+
+			if len(response) != 2:
+				raise Exception('response has an unexpected number of fields')
+			if 'error' not in response:
+				raise Exception('response is missing required error field')
+			if 'result' not in response:
+				raise Exception('response is missing required result field')
+			if response['error'] is not None:
+				raise Exception(response['error'])
+			return response['result']
+
+anki = AnkiConnect()
+result = anki.invoke('deckNames')
 print(result)
